7683615d1b8be5cef7f5b6f1fd3607d744a1802f
unknown <liammurph1998@gmail.com>
2021-11-07 22:32:17 -0500

    adding the body, contact and footer components. Also fixed stuff. This should have been many different commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { BodyComponent } from './body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    FooterComponent,
+    ContactComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule
